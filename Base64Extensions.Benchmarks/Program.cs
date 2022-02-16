0457681aee6fd9a070f4dedf82f1ec12b2e7eabd
Spandan Garg <spgarg@microsoft.com>
2022-02-16 11:29:53 -0700

    PERF: Optimize BaseConvert.cs

--- Base64Extensions.Benchmarks/Program.cs
+++ Base64Extensions.Benchmarks/Program.cs
@@ -1,7 +1,18 @@
 ﻿using BenchmarkDotNet.Running;
+using System;
+using System.Security.Cryptography;
 
 namespace Base64Extensions.Benchmarks
 {
+    class DeterministicRandomGenerator : RandomNumberGenerator
+    {
+        Random r = new Random(0);
+        public override void GetBytes(byte[] data)
+        {
+            r.NextBytes(data);
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
